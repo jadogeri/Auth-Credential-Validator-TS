d5e1efa969292c73fa6d876a1c0753485913e0d2
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-07-09 19:15:40 -0500

    Added regex.ts

---
+++ src/data/regex.ts
@@ -0,0 +1,6 @@
+const emailRegex : RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const usernameRegex : RegExp = /^[a-zA-Z0-9]{4,16}$/;
+const passwordRegex : RegExp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*-]).{8,20}$/
+
+
+export {emailRegex, usernameRegex, passwordRegex}
