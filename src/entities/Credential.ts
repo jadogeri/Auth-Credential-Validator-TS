e8225352e607f602d0b0af0e29f33127c90d2926
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-07-09 19:21:01 -0500

    Implemented Credential class **Credential.ts**

---
+++ src/entities/Credential.ts
@@ -0,0 +1,47 @@
+export class Credential {
+    username: string;
+    email: string;
+    password: string;
+
+    constructor(username: string = "", email: string = "", password: string = "") {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    getEmail(): string {
+        return this.email;
+    }
+
+    getUsername(): string | undefined {
+        return this.username || undefined;
+    }
+
+    getPassword(): string | undefined {
+        return this.password || undefined;
+    }
+
+    setEmail(email: string): void {
+        if (email === undefined || email === null) {
+            this.email = "";
+        } else {
+            this.email = email;
+        }
+    }
+
+    setUsername(username: string): void {
+        if (username === undefined || username === null) {
+            this.username = "";
+        } else {
+            this.username = username;
+        }
+    }
+
+    setPassword(password: string): void {
+        if (password === undefined || password === null) {
+            this.password = "";
+        } else {
+            this.password = password;
+        }
+    }
+}
