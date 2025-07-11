3473e631079bb1fe76fc270aaf8d1caae05d7dd6
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-07-11 11:14:34 -0500

    Updated test suites in validateEmatil.test.ts

--- src/data/regex.ts
+++ src/data/regex.ts
@@ -1,4 +1,4 @@
-const emailRegex : RegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const emailRegex : RegExp = /^(?!.*\.\.)(?!^\.)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/// /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 const usernameRegex : RegExp = /^[a-zA-Z0-9]{4,16}$/;
 const passwordRegex : RegExp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*-]).{8,20}$/
 
